452a98b0cb65e0bdade759583f366ec0dc781b28
Ritik Raj <99108259+theritik01@users.noreply.github.com>
2022-08-05 19:06:16 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import Header from './components/header'
+// import Footer from './components/footer'
+import Category from './components/Category'
+// import NavBar from './components/NavBar';
+function App() {
+  return (
+    <>
+    <div>
+      <Category/>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
